c8d9f277b8d62fe305a7344388876fc64aef683a
84fcosta <84.fcosta@gmail.com>
2026-08-18 17:30:10 -0700

    Expose sourcing intelligence workspace

--- costa-gear-app/src/App.js
+++ costa-gear-app/src/App.js
@@ -1,1 +1,11 @@
-export { default } from "./LegacyApp";
+import LegacyApp from "./LegacyApp";
+import SourcingIntelligencePanel from "./components/SourcingIntelligencePanel";
+
+export default function App() {
+  return (
+    <>
+      <LegacyApp />
+      <SourcingIntelligencePanel />
+    </>
+  );
+}
