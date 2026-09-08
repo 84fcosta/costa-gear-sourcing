f65463577a2657d70b22dd6545eb793fedc0b9b2
84fcosta <84.fcosta@gmail.com>
2026-09-08 01:07:22 -0700

    Load final mobile dashboard overrides after app styles

--- costa-gear-app/src/index.js
+++ costa-gear-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import AuthGate from './AuthGate';
+import './dashboard-mobile-overrides.css';
 
 const LEGACY_HOST = 'costa-gear-sourcing.vercel.app';
 const CANONICAL_ORIGIN = 'https://ops.costagear.ca';
